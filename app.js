cf5df78b0d4965b12752138c5ec6c332e91415f6
PranjaliNu10 <51490690+Pranjaliyadav@users.noreply.github.com>
2024-08-29 11:22:25 +0530

    creating server

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("dsdafssssa")
+const http = require('http')
+
+
+
+const server = http.createServer((req, res) => {
+    console.log("here req", req.url, req.method, req.headers)
+    // process.exit()
+
+    //we are sending all this back to client
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<body><h1>Hello</h1></body>')
+    res.write('</html>')
+    res.end()
+
+}) //this returns a server
+
+server.listen(3000)
