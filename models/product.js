81ce3bccf1472da42915dd21429fe2208273e800
PranjaliNu10 <51490690+Pranjaliyadav@users.noreply.github.com>
2024-08-30 23:49:22 +0530

    using model and data to save file content and accessing

--- models/product.js
+++ models/product.js
@@ -1,16 +1,42 @@
-const products = []
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../util/path')
 
+
+const getProductsFromFile = cb => {
+    const p = path.join(
+        rootDir,
+        'data',
+        'products.json'
+    );
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        }
+        fileContent && cb(JSON.parse(fileContent));
+
+    });
+}
 module.exports = class Product {
     constructor(t) {
-        this.title = t
+        this.title = t;
     }
 
     save() {
-        products.push(this)
+        getProductsFromFile()
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err => {
+                console.log(err);
+            });
+        });
     }
 
-    static fetchAll() {
-        return products
+    static fetchAll(cb) {
+       getProductsFromFile(cb)
     }
-
-}
+};
