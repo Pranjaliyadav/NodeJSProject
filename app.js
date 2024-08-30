69ef861216a44bd3ed66118c26881b07350de662
PranjaliNu10 <51490690+Pranjaliyadav@users.noreply.github.com>
2024-08-30 12:44:00 +0530

    express and middleware basic

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-const http = require('http');
-const routes = require('./routes')
-const server = http.createServer(routes.requestHandler);
 
-server.listen(3000); 
+const express = require('express')
+
+const app = express() 
+
+//allows us to use multiple middleware function
+app.use((req, res, next)=>{
+    console.log("In the middleware")
+    next() //to go to next middleware fn in line
+})
+
+app.use((req, res, next) => {
+    console.log("In the another middleware")
+    res.send("<h1>Hello another</h1>")
+})
+
+app.listen(3000) 
