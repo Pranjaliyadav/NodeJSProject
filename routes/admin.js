ab4c194dd05a9a1dac3c32818526c1c822bdadc5
PranjaliNu10 <51490690+Pranjaliyadav@users.noreply.github.com>
2024-08-30 17:29:28 +0530

    different way to give directory root path, added styles

--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,11 @@ const express = require('express')
 const path = require('path')
 const router = express.Router()
 
+const rootDir = require('../utils/path')
+
 router.get('/add-product', (req, res, next) => {
     console.log("In random page")
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 })
 
 router.post('/add-product', (req, res, next) => {
