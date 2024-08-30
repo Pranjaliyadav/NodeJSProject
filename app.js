2c80e74c9246555ffa7468c3b6ee3a4c67b37991
PranjaliNu10 <51490690+Pranjaliyadav@users.noreply.github.com>
2024-08-30 12:59:35 +0530

    middle ware routings

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use('/', (req, res, next) => {
-    console.log("Alwys runs")
-    next()
-})
+app.use(bodyParser.urlencoded({extended : false}))
+
 app.use('/add-product', (req, res, next) => {
     console.log("In add product page")
     res.send('<form action="/product" method = "POST" ><input type="text" name=  "title"><button type="submit">Submit</button></form>')
 })
 
-app.use('/product', (req, res, next) => {
+//app.get for fiktering get request, app.post for post request
+
+app.post('/product', (req, res, next) => {
     console.log("In product page", req.body)
+    // res.send("<h1>Hello another</h1>")
     res.redirect('/')
-    res.send("<h1>Hello another</h1>")
 })
 
 app.use('/', (req, res, next) => {
