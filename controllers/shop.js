e547eb588d9a405aee0d5b36094d579a536912a0
PranjaliNu10 <122367409+PranjaliNu10@users.noreply.github.com>
2024-09-02 18:37:07 +0530

    basic adding to cart done

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-
+const CartModel = require('../models/cart')
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/product-list', {
@@ -40,6 +40,9 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) =>{
   const prodId = req.body.productId
+  Product.findById(prodId, (product)=>{
+    CartModel.addProduct(prodId, product.price)
+  })
   console.log("here productId", prodId)
   res.redirect('/cart')
 }
--- models/cart.js
+++ models/cart.js
@@ -1,9 +1,49 @@
-module.exports = class Cart  {
-    constructor() {
-        this.products = []
-        this.totalPrice = 0
+const fs = require('fs')
+const path = require('path')
+const rootDir = require('../util/path')
+
+const p = path.join(
+    rootDir,
+    'data',
+    'cart.json'
+)
+
+
+module.exports = class Cart {
+    static addProduct(id, productPrice) {
+        //fetch the previous cart
+        fs.readFile(p, (err, fileContent) => {
+            let cart = { products: [], totalPrice: 0 }
+            if (!err) {
+                cart = JSON.parse(fileContent)
+            }
+            //analyze the cart => find existing product
+            const existingProductIndex = cart.products.findIndex(prod => prod.id === id)
+            const existingProduct = cart.products[existingProductIndex]
+            let updatedProduct
+            //add new product/ incr quantity
+
+            if (existingProduct) {
+                updatedProduct = { ...existingProduct }
+                updatedProduct.qty = updatedProduct.qty + 1
+                cart.products = [...cart.products]
+                cart.products[existingProductIndex] = updatedProduct
+            }
+
+            else {
+                updatedProduct = { id: id, qty: 1 }
+                cart.products = [...cart.products, updatedProduct]
+            }
+
+            cart.totalPrice = cart.totalPrice + +productPrice
+            fs.writeFile(p, JSON.stringify(cart), err => {
+                console.log("here error adding in cart", err)
+            })
+        })
+
+
 
     }
 
-    
+
 }
