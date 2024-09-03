808ffa69d03f31086ea02ecf5decd43f62d06b2c
PranjaliNu10 <122367409+PranjaliNu10@users.noreply.github.com>
2024-09-04 01:10:08 +0530

    schema for prod table

--- models/product.js
+++ models/product.js
@@ -4,8 +4,27 @@ const sequelize = require('../util/database')
 
 //creating Product table
 const Product = sequelize.define('product', {
-  id : {
-    type : SequelizeInstance.INTEGER,
-    autoCom
+  id: {
+    type: SequelizeInstance.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title:
+  {
+    type: SequelizeInstance.STRING,
+    allowNull: false
+  },
+  price: {
+    type: SequelizeInstance.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: SequelizeInstance.STRING,
+    allowNull: false
+  },
+  description: {
+    type: SequelizeInstance.STRING,
+    allowNull: false
   }
 })
