2d79fa205c5faf053e1f81151a7e47b0ee61d5d6
PranjaliNu10 <122367409+PranjaliNu10@users.noreply.github.com>
2024-09-04 19:35:25 +0530

    minor

--- app.js
+++ app.js
@@ -18,21 +18,42 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use((req, res, next) => {
+    UserModel.findByPk(1)
+        .then(user => {
+            req.user = user
+            next()
+        })
+        .catch(err => console.log(err))
+})
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-ProductModel.belongsTo(UserModel,{constraints : true, onDelete : 'CASCADE'})
+
+
+ProductModel.belongsTo(UserModel, { constraints: true, onDelete: 'CASCADE' })
 //these params define that if anything happend sto user record, product related record will also be affected
 
 UserModel.hasMany(ProductModel)
 
 
 //sync table with db
-sequelize.sync({force : true}).then(result => {
-    app.listen(3000);
-})
+sequelize
+    // .sync({force : true})
+    .sync()
+    .then(result => {
+        return UserModel.findByPk(1)
+    })
+    .then(user => {
+        if (!user) {
+            return UserModel.create({ name: 'Pranjali', email: 'test@tes.com' })
+        }
+        return user
+    })
+    .then(user => {
+        app.listen(3000);
+    })
     .catch(error => console.error(error))
 
