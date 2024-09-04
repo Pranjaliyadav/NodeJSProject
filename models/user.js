6e565961586a2dcfe5b1bf0f3fb99e487cea8b8f
PranjaliNu10 <122367409+PranjaliNu10@users.noreply.github.com>
2024-09-04 14:17:00 +0530

    user model crreatd

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const SequelizeInstance = require('sequelize')
+
+const sequelize = require('../util/database')
+
+//creating Product table
+const User = sequelize.define('user', {
+    id: {
+        type: SequelizeInstance.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name:
+    {
+        type: SequelizeInstance.STRING,
+        allowNull: false
+    },
+    email:
+    {
+        type: SequelizeInstance.STRING,
+        allowNull: false
+    },
+   
+})
+
+module.exports = User
+
+//id, name, email
