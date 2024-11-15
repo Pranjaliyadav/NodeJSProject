a8c8f667f2f6f56d536bced1d5d3dafccdefa92c
Pranjali Yadav <51490690+Pranjaliyadav@users.noreply.github.com>
2024-11-15 12:34:38 +0530

    delete things related to sequelize

--- util/database.js
+++
@@ -1,6 +0,0 @@
-const SequelizeInstance = require('sequelize')
-const sequelize = new SequelizeInstance('node-complete', 'root', 'password',
-    { dialect: 'mysql', host: 'localhost' }
-)
-
-module.exports = sequelize
