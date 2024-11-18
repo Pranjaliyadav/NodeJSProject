933b5045e968faf39bd0ab6bec12f9925f75d71e
Pranjali Yadav <51490690+Pranjaliyadav@users.noreply.github.com>
2024-11-19 00:03:49 +0530

    users model setup

--- app.js
+++ app.js
@@ -14,37 +14,54 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-// const User = require('./models/user')
+const User = require('./models/user')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use((req, res, next) => {
-//     User.findById('6738b57f5e3b179959b57467')
-//     .then(user => {
-//         if (!user) {
-//             throw new Error("User not found");
-//         }
-//         req.user = new User(user.name, user.email, user.cart, user._id);
-//         next();
-//     })
-//     .catch(err => {
-//         console.error("Error fetching user:", err);
-//         next(err);
-//     });
+app.use((req, res, next) => {
+    User.findById('673b8852a7eaf1f84ccdc05b')
+        .then(user => {
+            if (!user) {
+                throw new Error("User not found");
+            }
+            //full mongoose user, so can use all fn
+            req.user = user
+            next();
+        })
+        .catch(err => {
+            console.error("Error fetching user:", err);
+            next(err);
+        });
 
 
-// })
+})
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 mongoose.connect('mongodb+srv://yadavpranjali1223:xfhsWKnmLqenYFRX@cluster0.epngj.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')
-.then( result => {
-    app.listen(3000)
-    console.log("connected to mongoose")
-}
-    
-)
-.catch(err => {
-    console.log("error connecting to mongoose", err)
-})
+    .then(result => {
+        //find user if its already there, dont create new users again and again
+        User.findOne().then(
+            user => {
+                if (!user) {
+
+                    const user = new User({
+                        name: 'Pranjali',
+                        email: 'prnj@s.so',
+                        cart: {
+                            items: []
+                        }
+                    })
+                    user.save()
+                }
+            }
+        )
+        app.listen(3000)
+        console.log("connected to mongoose")
+    }
+
+    )
+    .catch(err => {
+        console.log("error connecting to mongoose", err)
+    })
--- models/user.js
+++ models/user.js
@@ -1,197 +1,223 @@
-const { getDb } = require('../util/database')
-const mongoDb = require('mongodb')
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const userSchema =new Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true
+    },
+    cart : {
+        items : [
+            {
+             productId : {type : Schema.Types.ObjectId, required : true},
+             quantity : {type : Number, required : true}
+            }]
+    },
+})
+
+//collection name - users
+module.exports = mongoose.model('User', userSchema)
 
 
-class User {
-    constructor(username, email, cart, id) {
-        this.name = username
-        this.email = email
-        this.cart = cart // {items  : []}
-        this._id = id
-    }
+// const { getDb } = require('../util/database')
+// const mongoDb = require('mongodb')
 
-    save() {
-        const db = getDb()
-        try {
-            const response = db.collection('users')
-                .insertOne(this)
-            console.log("saved user", response)
-            return response
-        }
-        catch (err) {
-            console.log("error saving user", err)
-        }
-    }
 
-    async addToCart(product) {
-        let cartProductFoundIndex = this.cart?.items?.findIndex(
-            cp => 
-                cp.productId.toString() === product._id.toString()
+// class User {
+//     constructor(username, email, cart, id) {
+//         this.name = username
+//         this.email = email
+//         this.cart = cart // {items  : []}
+//         this._id = id
+//     }
+
+//     save() {
+//         const db = getDb()
+//         try {
+//             const response = db.collection('users')
+//                 .insertOne(this)
+//             console.log("saved user", response)
+//             return response
+//         }
+//         catch (err) {
+//             console.log("error saving user", err)
+//         }
+//     }
+
+//     async addToCart(product) {
+//         let cartProductFoundIndex = this.cart?.items?.findIndex(
+//             cp => 
+//                 cp.productId.toString() === product._id.toString()
             
-        ) 
+//         ) 
         
     
-        let newQty = 1
-        const updatedCartItems = this.cart?.items ? [...this.cart?.items] : []
+//         let newQty = 1
+//         const updatedCartItems = this.cart?.items ? [...this.cart?.items] : []
 
-        if (cartProductFoundIndex >= 0) {
-            newQty = this.cart.items[cartProductFoundIndex].quantity + 1
-            updatedCartItems[cartProductFoundIndex].quantity = newQty
-        }
-        else {
-            updatedCartItems.push({
-                productId: new mongoDb.ObjectId(product._id),
-                quantity: newQty
-            })
-        }
-        const updatedCart = { items: updatedCartItems }
+//         if (cartProductFoundIndex >= 0) {
+//             newQty = this.cart.items[cartProductFoundIndex].quantity + 1
+//             updatedCartItems[cartProductFoundIndex].quantity = newQty
+//         }
+//         else {
+//             updatedCartItems.push({
+//                 productId: new mongoDb.ObjectId(product._id),
+//                 quantity: newQty
+//             })
+//         }
+//         const updatedCart = { items: updatedCartItems }
 
-        const db = getDb()
-        try {
-            const response = await db.collection('users')
-                .updateOne(
-                    { _id: new mongoDb.ObjectId(this._id) },
-                    { $set: { cart: updatedCart } })
-            return response
-        }
-        catch (err) {
-            console.log("error updating cart", err)
-        }
-
-
-    }
-
-    async getCart() {
-        const db = getDb()
-        try {
-            const productIds = this.cart.items.map(rec => rec.productId)
-
-            const cartItems = await db.collection('products').find({ _id: { $in: productIds } })
-                .toArray()
-            if (cartItems) {
-                return cartItems.map(p => {
-                    return {
-                        ...p, quantity: this.cart.items.find(i => {
-                            return i.productId.toString() === p._id.toString()
-                        }).quantity,
-                        userId : this._id
-                    }
-                })
-            }
-            else {
-                return []
-            }
-
-        }
-        catch (err) {
-            console.log("error getting cart", err)
-        }
+//         const db = getDb()
+//         try {
+//             const response = await db.collection('users')
+//                 .updateOne(
+//                     { _id: new mongoDb.ObjectId(this._id) },
+//                     { $set: { cart: updatedCart } })
+//             return response
+//         }
+//         catch (err) {
+//             console.log("error updating cart", err)
+//         }
 
 
-    }
+//     }
 
-    async deleteFromCart(productId) {
-        const db = getDb()
-        const updatedCartItem = this.cart.items.filter(rec => rec.productId.toString() !== productId.toString())
+//     async getCart() {
+//         const db = getDb()
+//         try {
+//             const productIds = this.cart.items.map(rec => rec.productId)
 
-        return await db.collection('users')
-            .updateOne(
-                { _id: new mongoDb.ObjectId(this._id) },
-                { $set: { cart: { items: updatedCartItem } } })
+//             const cartItems = await db.collection('products').find({ _id: { $in: productIds } })
+//                 .toArray()
+//             if (cartItems) {
+//                 return cartItems.map(p => {
+//                     return {
+//                         ...p, quantity: this.cart.items.find(i => {
+//                             return i.productId.toString() === p._id.toString()
+//                         }).quantity,
+//                         userId : this._id
+//                     }
+//                 })
+//             }
+//             else {
+//                 return []
+//             }
 
-    }
-    async addOrder() {
-        const db = getDb()
-       return await this.getCart()
-            .then(async products => {
-
-                const order = {
-                    items: products,
-                    user: {
-                        _id: new mongoDb.ObjectId(this._id),
-                        email: this.email,
-                        name: this.name
-                    }
-                }
-                try {
-                    const response = await db.collection('orders')
-                        .insertOne(order)
-
-                    if (response) {
-                        this.cart = { items: [] }
-                        try {
-                            const response = db.collection('users')
-                                .updateOne(
-                                    { _id: new mongoDb.ObjectId(this._id) },
-                                    { $set: { cart: { items: [] } } }
-                                )
-
-                            return response
-                        }
-                        catch (err) {
-                            console.log("error updating cart", err)
-                        }
-                    }
-                }
-                catch (err) {
-                    console.log("error adding cart to order", err)
-                }
-            })
-
-    }
-
-    async getOrdersForUser() {
-        const db = getDb()
-        try{
-            const response = await db.collection('orders')
-            .find({'user._id' : new mongoDb.ObjectId(this._id)})
-            .toArray()
-            return response
-        }
-        catch(err){
-            console.log("error getting order",err)
-        }
-    }
-
-    // async deleteQuantityFromCart(productId) {
-    //     const db = getDB()
-    //     const updatedCartItem = this.cart.items.findOne(rec => rec.productId.toString() === productId.toString())
-    //     let cartToSave = []
-    //     if (updatedCartItem) {
-    //         let currQty = updatedCartItem.quantity
-    //         currQty--
-    //         if (currQty > 0) {
-
-    //         }
-    //         else {
-    //             cartToSave = this.cart.items.filter(rec => rec.productId.toString() !== productId.toString())
-
-    //         }
-
-    //         try {
-
-    //         }
-    //         catch (err) {
-    //             console.log("error deleting cart item", err)
-    //         }
-
-    //     }
-    // }
-    static async findById(userId) {
-        const db = getDb()
-        try {
-            const response = await db.collection('users')
-                .findOne({ _id: new mongoDb.ObjectId(userId) })
-            console.log("saved user", response)
-            return response
-        }
-        catch (err) {
-            console.log("error finding user", err)
-        }
-    }
-}
+//         }
+//         catch (err) {
+//             console.log("error getting cart", err)
+//         }
 
 
-module.exports = User
+//     }
 
-//id, name, email
+//     async deleteFromCart(productId) {
+//         const db = getDb()
+//         const updatedCartItem = this.cart.items.filter(rec => rec.productId.toString() !== productId.toString())
+
+//         return await db.collection('users')
+//             .updateOne(
+//                 { _id: new mongoDb.ObjectId(this._id) },
+//                 { $set: { cart: { items: updatedCartItem } } })
+
+//     }
+//     async addOrder() {
+//         const db = getDb()
+//        return await this.getCart()
+//             .then(async products => {
+
+//                 const order = {
+//                     items: products,
+//                     user: {
+//                         _id: new mongoDb.ObjectId(this._id),
+//                         email: this.email,
+//                         name: this.name
+//                     }
+//                 }
+//                 try {
+//                     const response = await db.collection('orders')
+//                         .insertOne(order)
+
+//                     if (response) {
+//                         this.cart = { items: [] }
+//                         try {
+//                             const response = db.collection('users')
+//                                 .updateOne(
+//                                     { _id: new mongoDb.ObjectId(this._id) },
+//                                     { $set: { cart: { items: [] } } }
+//                                 )
+
+//                             return response
+//                         }
+//                         catch (err) {
+//                             console.log("error updating cart", err)
+//                         }
+//                     }
+//                 }
+//                 catch (err) {
+//                     console.log("error adding cart to order", err)
+//                 }
+//             })
+
+//     }
+
+//     async getOrdersForUser() {
+//         const db = getDb()
+//         try{
+//             const response = await db.collection('orders')
+//             .find({'user._id' : new mongoDb.ObjectId(this._id)})
+//             .toArray()
+//             return response
+//         }
+//         catch(err){
+//             console.log("error getting order",err)
+//         }
+//     }
+
+//     // async deleteQuantityFromCart(productId) {
+//     //     const db = getDB()
+//     //     const updatedCartItem = this.cart.items.findOne(rec => rec.productId.toString() === productId.toString())
+//     //     let cartToSave = []
+//     //     if (updatedCartItem) {
+//     //         let currQty = updatedCartItem.quantity
+//     //         currQty--
+//     //         if (currQty > 0) {
+
+//     //         }
+//     //         else {
+//     //             cartToSave = this.cart.items.filter(rec => rec.productId.toString() !== productId.toString())
+
+//     //         }
+
+//     //         try {
+
+//     //         }
+//     //         catch (err) {
+//     //             console.log("error deleting cart item", err)
+//     //         }
+
+//     //     }
+//     // }
+//     static async findById(userId) {
+//         const db = getDb()
+//         try {
+//             const response = await db.collection('users')
+//                 .findOne({ _id: new mongoDb.ObjectId(userId) })
+//             console.log("saved user", response)
+//             return response
+//         }
+//         catch (err) {
+//             console.log("error finding user", err)
+//         }
+//     }
+// }
+
+
+// module.exports = User
+
+// //id, name, email
