7fee947c1f3dfbbeb67990eca9b787d38c853918
Pranjali Yadav <51490690+Pranjaliyadav@users.noreply.github.com>
2024-11-23 23:44:06 +0530

    added route protection

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,6 +2,7 @@ const { error } = require('console');
 const mongoDB = require('mongodb')
 const Product = require('../models/product');
 exports.getAddProduct = (req, res, next) => {
+
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
