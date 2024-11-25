aaa29fa640d6eb964ae08a4b7da3a0c118a3b348
Pranjali Yadav <51490690+Pranjaliyadav@users.noreply.github.com>
2024-11-25 22:06:09 +0530

    password validation

--- routes/auth.js
+++ routes/auth.js
@@ -6,13 +6,16 @@ const router = express.Router();
 require('dotenv').config();
 const authController = require('../controllers/auth')
 
-const { check } = require('express-validator')
+const { check, body } = require('express-validator')
 
 router.get('/login', authController.getLogin)
 router.post('/login', authController.postLogin)
 router.post('/logout', authController.postLogout)
 router.get('/signup', authController.getSignup)
-router.post('/signup',check('email')
+router.post('/signup',
+    //put all validation in array checking keys of your form
+    [
+        check('email')
 .isEmail()
 .withMessage('Please enter a valid email')
 .custom((value, {req})=>{
@@ -20,7 +23,12 @@ router.post('/signup',check('email')
         throw new Error('This email address is forbidden')
     }
     return true
-})
+}),
+body('password',
+'Please enter a valid password with at least 6 characters.' //this is default error message
+)
+.isLength({min : 6})
+]
 , authController.postSignup)
 router.get('/reset', authController.getPasswordReset)
 router.post('/reset', authController.postPasswordReset)
